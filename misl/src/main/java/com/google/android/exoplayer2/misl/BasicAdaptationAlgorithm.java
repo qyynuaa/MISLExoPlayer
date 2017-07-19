d76b2b0450dd903dcc4af41940817b0c7006d137
Noel Bourke <undergroundquizscene@gmail.com>
2017-07-19 13:52:37 +0100

    Add Factory to BasicAdaptationAlgorithm and Use AlgorithmListener
    I’ve replaced the BandwidthMeter with an AlgorithmListener, and now the
    algorithm is using just the delivery rate of the last segment as its
    estimate of the throughput.

--- misl/src/main/java/com/google/android/exoplayer2/misl/BasicAdaptationAlgorithm.java
+++ misl/src/main/java/com/google/android/exoplayer2/misl/BasicAdaptationAlgorithm.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.source.TrackGroup;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
 
 import static com.google.android.exoplayer2.upstream.BandwidthMeter.NO_ESTIMATE;
 
@@ -14,19 +15,40 @@ import static com.google.android.exoplayer2.upstream.BandwidthMeter.NO_ESTIMATE;
  */
 public class BasicAdaptationAlgorithm extends AdaptationAlgorithm {
 
+    public static final class Factory implements AdaptationAlgorithm.Factory {
+
+        private final AlgorithmListener algorithmListener;
+
+        public Factory(AlgorithmListener algorithmListener) {
+            this.algorithmListener = algorithmListener;
+        }
+
+        /**
+         * Creates a new algorithm.
+         *
+         * @param group  The {@link TrackGroup}.
+         * @param tracks The indices of the selected tracks within the {@link TrackGroup}, in any order.
+         * @return The created algorithm.
+         */
+        @Override
+        public AdaptationAlgorithm createAlgorithm(TrackGroup group, int... tracks) {
+            return new BasicAdaptationAlgorithm(group, tracks, algorithmListener);
+        }
+    }
+
     private static final String TAG = "BasicAlgorithm";
 
-    private final BandwidthMeter bandwidthMeter;
+    private final AlgorithmListener algorithmListener;
 
     private int reason;
 
-    public BasicAdaptationAlgorithm(TrackGroup group, int[] tracks, BandwidthMeter bandwidthMeter) {
+    public BasicAdaptationAlgorithm(TrackGroup group, int[] tracks, AlgorithmListener algorithmListener) {
         super(group, tracks);
-        this.bandwidthMeter = bandwidthMeter;
+        this.algorithmListener = algorithmListener;
     }
 
     public int determineIdealIndex(long bufferedDurationUs) {
-        long bitrateEstimate = bandwidthMeter.getBitrateEstimate();
+        double bitrateEstimate = algorithmListener.lastDeliveryRate();
         Log.d(TAG, "Bitrate estimate = " + bitrateEstimate);
         int selectedIndex = getTracks().length - 1;
 
