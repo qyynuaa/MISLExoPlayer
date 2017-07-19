05dd40938616361374e2c4b4a53acfc2b4cc3afe
Noel Bourke <undergroundquizscene@gmail.com>
2017-07-19 11:59:02 +0100

    Replace DefaultBandwidthMeter References with TransferListener References
    Since I want to replace the bandwidth meter with a
    `DefaultAlgorithmInfoProvider`, I need to change these variable types.
    
    To allow for more flexibility, I’ve gone with `TransferListener` –
    that’s what this parameter is used as anyway when passed to
    `DefaultDataSourceFactory`.

--- demo/src/main/java/com/google/android/exoplayer2/demo/DemoApplication.java
+++ demo/src/main/java/com/google/android/exoplayer2/demo/DemoApplication.java
@@ -17,10 +17,10 @@ package com.google.android.exoplayer2.demo;
 
 import android.app.Application;
 import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.upstream.HttpDataSource;
+import com.google.android.exoplayer2.upstream.TransferListener;
 import com.google.android.exoplayer2.util.Util;
 
 /**
@@ -36,12 +36,12 @@ public class DemoApplication extends Application {
     userAgent = Util.getUserAgent(this, "ExoPlayerDemo");
   }
 
-  public DataSource.Factory buildDataSourceFactory(DefaultBandwidthMeter bandwidthMeter) {
+  public DataSource.Factory buildDataSourceFactory(TransferListener bandwidthMeter) {
     return new DefaultDataSourceFactory(this, bandwidthMeter,
         buildHttpDataSourceFactory(bandwidthMeter));
   }
 
-  public HttpDataSource.Factory buildHttpDataSourceFactory(DefaultBandwidthMeter bandwidthMeter) {
+  public HttpDataSource.Factory buildHttpDataSourceFactory(TransferListener bandwidthMeter) {
     return new DefaultHttpDataSourceFactory(userAgent, bandwidthMeter);
   }
 
