6b0b916089c49d5b8314f9180a13c75749fcb062
Noel Bourke <undergroundquizscene@gmail.com>
2017-07-20 15:11:44 +0100

    Add Factory to ElasticAdaptationAlgorithm
    I had forgotten that AdaptationAlgorithms need to use a factory for
    creation, as they’ll be given the group and tracks parameters by an
    AlgorithmTrackSelection.

--- misl/src/main/java/com/google/android/exoplayer2/misl/ElasticAdaptationAlgorithm.java
+++ misl/src/main/java/com/google/android/exoplayer2/misl/ElasticAdaptationAlgorithm.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import com.google.android.exoplayer2.source.TrackGroup;
 
+import static com.google.android.exoplayer2.misl.AlgorithmInfoProvider.DATA_NOT_AVAILABLE;
 import static java.lang.Math.min;
 
 /**
@@ -12,28 +13,58 @@ import static java.lang.Math.min;
 
 public class ElasticAdaptationAlgorithm extends AdaptationAlgorithm {
 
-    private static final String TAG = "Elastic";
+    public static final class Factory implements AdaptationAlgorithm.Factory {
 
-    private static final int DEFAULT_AVERAGE_WINDOW = 5;
-    private static final double DEFAULT_K_P = 0.01;
-    private static final double DEFAULT_K_I = 0.001;
+        private static final int DEFAULT_AVERAGE_WINDOW = 5;
+        private static final double DEFAULT_K_P = 0.01;
+        private static final double DEFAULT_K_I = 0.001;
+
+        private final int averageWindow;
+        private final double k_p;
+        private final double k_i;
+
+        private final AlgorithmInfoProvider infoProvider;
+
+        public Factory(AlgorithmInfoProvider infoProvider) {
+            this(infoProvider, DEFAULT_AVERAGE_WINDOW, DEFAULT_K_P,
+                DEFAULT_K_I);
+        }
+
+        /**
+         *
+         * @param infoProvider An object the algorithm can get necessary info from.
+         * @param averageWindow The number of past samples to include in rate estimation.
+         * @param k_p The first algorithm constant.
+         * @param k_i The second algorithm constant.
+         */
+        public Factory(AlgorithmInfoProvider infoProvider, int averageWindow, double k_p, double k_i) {
+            this.infoProvider = infoProvider;
+            this.averageWindow = averageWindow;
+            this.k_p = k_p;
+            this.k_i = k_i;
+        }
+
+        /**
+         * Creates a new algorithm.
+         *
+         * @param group  The {@link TrackGroup}.
+         * @param tracks The indices of the selected tracks within the {@link TrackGroup}, in any order.
+         * @return The created algorithm.
+         */
+        @Override
+        public AdaptationAlgorithm createAlgorithm(TrackGroup group, int... tracks) {
+            return new ElasticAdaptationAlgorithm(group, tracks, infoProvider,
+                    averageWindow, k_p, k_i);
+        }
+    }
+
+    private static final String TAG = "Elastic";
 
     private AlgorithmInfoProvider infoProvider;
     private int averageWindow;
     private double k_p;
     private double k_i;
     private double staticAlgParameter;
-
-    /**
-     * Creates a new algorithm.
-     *
-     * @param group  The {@link TrackGroup}.
-     * @param tracks The indices of the selected tracks in group, in order of decreasing bandwidth.
-     * @param infoProvider An object the algorithm can get necessary info from.
-     */
-    public ElasticAdaptationAlgorithm(TrackGroup group, int[] tracks, AlgorithmInfoProvider infoProvider) {
-        this(group, tracks, infoProvider, DEFAULT_AVERAGE_WINDOW, DEFAULT_K_P, DEFAULT_K_I);
-    }
 
     /**
      * Creates a new algorithm.
@@ -82,6 +113,8 @@ public class ElasticAdaptationAlgorithm extends AdaptationAlgorithm {
             for (int i = 0; i <= workingAverageWindow; i++) {
                 rateSamples[i] = infoProvider.deliveryRate(lastChunkIndex - i);
             }
+
+            Log.d(TAG, String.format("rateSamples length is %d", rateSamples.length));
 
             double averageRateEstimate = new Average(Average.Pythagorean.HARMONIC, rateSamples).value();
 
