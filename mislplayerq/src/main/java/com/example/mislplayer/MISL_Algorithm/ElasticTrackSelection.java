e42d1553228820c28c0ee013b36ca7387161a816
Noel Bourke <undergroundquizscene@gmail.com>
2017-07-28 11:07:30 +0100

    Tidy up ElasticTrackSelection
    I’m starting with ElasticTrackSelection for moving the sample
    processing to the listener, as it’s not very complicated and I
    understand it well.
    
    To start, I’ve removed unnecessary clutter: the comment explaining the
    variables used in the C++ version of the algorithm, the unused
    variables, and the early return inherited from the C++ code.
    
    The early return should be redundant, as the
    algorithmListener.chunkDataNotAvailable() check should guarantee that
    the second check fails.

--- mislplayerq/src/main/java/com/example/mislplayer/MISL_Algorithm/ElasticTrackSelection.java
+++ mislplayerq/src/main/java/com/example/mislplayer/MISL_Algorithm/ElasticTrackSelection.java
@@ -148,56 +148,27 @@ public class ElasticTrackSelection extends AlgorithmTrackSelection {
      * @return The index (in sorted order) of the track to switch to.
      */
     private int doRateAdaptation(long bufferedDurationUs) {
-        /*
-                Brief explanation of used variables:
-                total_size - size (in bytes) of the last downloaded segment
-                time - time needed to download the last segment
-                rep->bandwidth - average (nominal) representation rate (bps)
-                bitrate - actual representation rate (kbps)
-                bytes_per_sec - throughput for the last segment
-                group->current_downloaded_segment_duration - segment duration (in ms)
-                group->max_cached_segments - max number of cached segments - total amount of buffer is twice this size (because there is also a playback buffer: total = play + cache)
-                group->max_buffer_segments - same as above
-                group->active_rep_index - current index of downloaded segment
-                group->download_segment_index + 1 - number of downloaded segments
-        */
-
         if (algorithmListener.chunkDataNotAvailable()) {
             return lowestBitrateIndex();
         }
 
-
-        final int segmentIndex = algorithmListener.lastChunkIndex();
-
-        final double totalSizeBytes = algorithmListener.lastByteSize();
-        final double deliveryRateKbps = algorithmListener.lastDeliveryRateKbps();
         final double downloadTimeS = algorithmListener.lastLoadDurationMs() / 1E3;
-        final double lastSegmentDurationS = algorithmListener.lastChunkDurationMs() / 1E3;
+        final double elasticTargetQueueS = algorithmListener.getMaxBufferMs() / 1E3;
+        final double queueLengthS = bufferedDurationUs / 1E6;
 
-        if (totalSizeBytes == 0 || deliveryRateKbps == 0 || lastSegmentDurationS == 0) {
-            // skipping rate adaptation – log details and return
-            return -1;
-        } else {
-            final double elasticTargetQueueS = algorithmListener.getMaxBufferMs() / 1E3;
+        int averageWindow = algorithmListener.getWindowSize(elasticAverageWindow);
+        double[] rateSamples = algorithmListener.getThroughputSamples(averageWindow);
+        double averageRateEstimate = new HarmonicAverage(averageWindow, rateSamples).value();
 
-            final double queueLengthS = bufferedDurationUs / 1E6;
+        staticAlgParameter += downloadTimeS * (queueLengthS - elasticTargetQueueS);
+        double targetRate = averageRateEstimate / (1 - k_p * queueLengthS - k_i * staticAlgParameter);
 
-            int averageWindow = algorithmListener.getWindowSize(elasticAverageWindow);
-            double[] rateSamples = algorithmListener.getThroughputSamples(averageWindow);
-
-            double averageRateEstimate = new HarmonicAverage(averageWindow, rateSamples).value();
-
-            staticAlgParameter += downloadTimeS * (queueLengthS - elasticTargetQueueS);
-
-            double targetRate = averageRateEstimate / (1 - k_p * queueLengthS - k_i * staticAlgParameter);
-
-            if (targetRate <= 0) {
-                targetRate = 0;
-            }
-
-            Log.d(TAG, String.format("targetRate = %f kbps", targetRate));
-
-            return findBestRateIndex(targetRate * 1000);
+        if (targetRate <= 0) {
+            targetRate = 0;
         }
+
+        Log.d(TAG, String.format("targetRate = %f kbps", targetRate));
+
+        return findBestRateIndex(targetRate * 1000);
     }
 }
