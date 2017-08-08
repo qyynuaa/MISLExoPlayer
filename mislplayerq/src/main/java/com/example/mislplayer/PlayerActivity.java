a283a9fb6bfcbfcd0359c733e60bdb211dc989f9
Noel Bourke <undergroundquizscene@gmail.com>
2017-08-08 14:22:42 +0100

    Use size-based estimation
    I have added a button to the algorithm chooser that allows you to
    choose the basic algorithm using size-based estimation.
    
    It works, and as expected, smaller thresholds give more variation in
    the rate than larger thresholds.
    
    I changed the text on the buttons to be in lowercase, as well, because
    I think it looks nicer.
    
    I have also deleted the TransitionalAlgorithmListener class, as it’s no
    longer needed, and the other SampleProcessor interface, as I’m not
    using it and it was confusing having two of them. I think I have
    deleted these before – they must have come back in a merge at some
    point.

--- mislplayerq/src/main/java/com/example/mislplayer/PlayerActivity.java
+++ mislplayerq/src/main/java/com/example/mislplayer/PlayerActivity.java
@@ -15,8 +15,10 @@ import com.example.mislplayer.sampling.ChunkBasedSampler;
 import com.example.mislplayer.sampling.ChunkLogger;
 import com.example.mislplayer.sampling.DefaultChunkLogger;
 import com.example.mislplayer.sampling.DefaultSampleProcessor;
+import com.example.mislplayer.sampling.SizeBasedSampler;
 import com.example.mislplayer.trackselection.ArbiterTrackSelection;
 import com.example.mislplayer.trackselection.BBA2TrackSelection;
+import com.example.mislplayer.trackselection.BasicTrackSelection;
 import com.example.mislplayer.trackselection.ElasticTrackSelection;
 import com.example.mislplayer.trackselection.OscarHTrackSelection;
 import com.google.android.exoplayer2.C;
@@ -201,6 +203,9 @@ public class PlayerActivity extends Activity implements View.OnClickListener,
             DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
             transferListener = bandwidthMeter;
             trackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        } else if (algorithmType == AdaptationAlgorithmType.BASIC_SIZE) {
+            transferListener = new SizeBasedSampler(sampleProcessor, 100_000);
+            trackSelectionFactory = new BasicTrackSelection.Factory(sampleProcessor);
         } else {
             ChunkBasedSampler chunkSampler = new ChunkBasedSampler(sampleProcessor, sampleProcessor);
             transferListener = chunkSampler;
