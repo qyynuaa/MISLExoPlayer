b45d8be09afca461c2442b50f21d0c517555faa4
Noel Bourke <undergroundquizscene@gmail.com>
2017-08-17 16:24:16 +0100

    Add Arbiter+ option for testing
    I’ve added the ability to select the Arbiter+ algorithm for testing
    from the algorithm chooser dialog.

--- mislplayerq/src/main/java/com/example/mislplayer/AdaptationAlgorithmType.java
+++ mislplayerq/src/main/java/com/example/mislplayer/AdaptationAlgorithmType.java
@@ -5,5 +5,5 @@ package com.example.mislplayer;
  */
 
 public enum AdaptationAlgorithmType {
-    BBA2, ARBITER, ELASTIC, OSCAR_H, BASIC_ADAPTIVE, BASIC_SIZE, BASIC_TIME
+    BBA2, ARBITER, ELASTIC, OSCAR_H, BASIC_ADAPTIVE, BASIC_SIZE, ARBITER_PLUS, BASIC_TIME
 }
